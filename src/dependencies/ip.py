c6412b38bd961a170e6ed2a774cb75b05bb88689
Eytes <yura.shamanov2000@bk.ru>
2025-03-20 15:48:54 +0200

    feature(ip dependencies): adding a dependency to get the ip of the original user who made the request

--- src/dependencies/ip.py
+++ src/dependencies/ip.py
@@ -20,8 +20,40 @@ def get_client_ip(request: Request) -> str:
     return request.client.host
 
 
-def is_trusted_ip(ip: Annotated[str, Depends(get_client_ip)]) -> str:
-    """Проверка, что ip адрес является доверенным"""
+def get_user_ip(request: Request) -> str | None:
+    """
+    Извлекает IP-адрес клиента из заголовка `X-Real-IP`.
+
+    **Параметры:**
+    - `request` (Request): Объект запроса FastAPI.
+
+    **Возвращаемое значение:**
+    - `str | None`: IP-адрес клиента, если заголовок `X-Real-IP` присутствует, иначе `None`.
+
+    **Описание:**
+    Функция проверяет заголовок `X-Real-IP` и возвращает IP-адрес клиента,
+    который был передан через прокси-сервер. Если заголовок отсутствует, возвращает `None`.
+    """
+    return request.headers.get("X-Real-IP")
+
+
+def is_trusted_ip(ip: Annotated[str, Depends(get_requesters_ip)]) -> str:
+    """
+    Проверяет, является ли IP-адрес клиента доверенным.
+
+    **Параметры:**
+    - `ip` (str): IP-адрес клиента, полученный через `get_requesters_ip()`.
+
+    **Возвращаемое значение:**
+    - `str`: IP-адрес клиента, если он находится в списке доверенных.
+
+    **Исключения:**
+    - `AccessDeniedHTTPException`: Вызывается, если IP-адрес не является доверенным.
+
+    **Описание:**
+    Функция проверяет, содержится ли переданный IP-адрес в списке доверенных `settings.trusted_ips`.
+    Если адрес не является доверенным, вызывается исключение `AccessDeniedHTTPException()`.
+    """
     if ip not in settings.trusted_ips:
         raise AccessDeniedHTTPException()
     return ip
